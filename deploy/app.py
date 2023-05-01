9c0bc317f018b4d064e082d0357e6a0d001ffbbf
Charu Khemka <72015637+charu1172@users.noreply.github.com>
2023-05-01 08:34:25 +0530

    Update app.py

--- deploy/app.py
+++ deploy/app.py
@@ -11,15 +11,16 @@ st.title("Food Recommendation System")
 st.text("Let us help you with ordering")
 # st.image("foood.jpg")
 
-## nav = st.sidebar.radio("Navigation",["Home","IF Necessary 1","If Necessary 2"])
+## nav = st.sidebar.radio("Navigation",["Home","IF Necessary 1",
+"If Necessary 2"])
 
 st.subheader("Whats your preference?")
 vegn = st.radio("Vegetables or none!",["veg","non-veg"],index = 1) 
 
 st.subheader("What Cuisine do you prefer?")
-cuisine = st.selectbox("Choose your favourite!",['Healthy Food', 'Snack', 'Dessert', 
-                                        'Japanese', 'Indian', 'French',
-                                       'Mexican', 'Italian', 'Chinese', 'Beverage', 'Thai'])
+cuisine = st.selectbox("Choose your favourite!",['Healthy Food', 
+    'Snack','Dessert', 'Japanese', 'Indian', 'French','Mexican', 
+   'Italian','Chinese','Beverage', 'Thai'])
 
 #RATING
 st.subheader("How well do you want the dish to be?")  
@@ -28,10 +29,12 @@ val = st.slider("from poor to the best!",0,10)
 food = pd.read_csv("input/food.csv")
 ratings = pd.read_csv("input/ratings.csv")
 combined = pd.merge(ratings, food, on='Food_ID')
-#ans = food.loc[(food.C_Type == cuisine) & (food.Veg_Non == vegn),['Name','C_Type','Veg_Non']]
+#ans = food.loc[(food.C_Type == cuisine) & 
+(food.Veg_Non == vegn),['Name','C_Type','Veg_Non']]
 
-ans = combined.loc[(combined.C_Type == cuisine) & (combined.Veg_Non == vegn)&
-                   (combined.Rating >= val),['Name','C_Type','Veg_Non']]
+ans = combined.loc[(combined.C_Type == cuisine) & 
+(combined.Veg_Non == vegn)&
+(combined.Rating >= val),['Name','C_Type','Veg_Non']]
 
 names = ans['Name'].tolist()
 x = np.array(names)
@@ -44,12 +47,14 @@ if bruh == True:
 
 
 ##### IMPLEMENTING RECOMMENDER ######
-dataset = ratings.pivot_table(index='Food_ID',columns='User_ID',values='Rating')
+dataset = ratings.pivot_table(index='Food_ID',
+               columns='User_ID',values='Rating')
 dataset.fillna(0,inplace=True)
 csr_dataset = csr_matrix(dataset.values)
 dataset.reset_index(inplace=True)
 
-model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+model = NearestNeighbors(metric='cosine', algorithm='brute', 
+                         n_neighbors=20, n_jobs=-1)
 model.fit(csr_dataset)
 
 def food_recommendation(Food_Name):
@@ -58,14 +63,16 @@ def food_recommendation(Food_Name):
     if len(FoodList):        
         Foodi= FoodList.iloc[0]['Food_ID']
         Foodi = dataset[dataset['Food_ID'] == Foodi].index[0]
-        distances , indices = model.kneighbors(csr_dataset[Foodi],n_neighbors=n+1)    
+        distances , indices = model.kneighbors(csr_dataset[Foodi],
+                                                 n_neighbors=n+1)    
         Food_indices = sorted(list(zip(indices.squeeze().tolist(),
-                                       distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
+        distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
         Recommendations = []
         for val in Food_indices:
             Foodi = dataset.iloc[val[0]]['Food_ID']
             i = food[food['Food_ID'] == Foodi].index
-            Recommendations.append({'Name':food.iloc[i]['Name'].values[0],'Distance':val[1]})
+            Recommendations.append({'Name':food.iloc[i]['Name']
+                                    .values[0],'Distance':val[1]})
         df = pd.DataFrame(Recommendations,index=range(1,n+1))
         return df['Name']
     else:
